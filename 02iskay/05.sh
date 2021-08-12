7e28e36a1c6a0887928ab3080388768a970a3cec
hellparker <82740039+hellparker@users.noreply.github.com>
2021-08-12 10:47:08 -0400

    Update 05.sh

--- 02iskay/05.sh
+++ 02iskay/05.sh
@@ -1,19 +1,8 @@
 #!/bin/bash
-
 ##05##
-
 ## COMPUTER NAME & HOST
-
-
-
 echo Arco > /etc/hostname
-
 echo "127.0.0.1 localhost" >> /etc/hosts
-
 echo "::1       localhost" >> /etc/hosts
-
 echo "127.0.1.1 Arco.localdomain Arco" >> /etc/hosts
-
 nano /etc/hosts
-
-
