d632570881a557e67f3026bfae05ba23c0c10bd9
hellparker <82740039+hellparker@users.noreply.github.com>
2021-08-12 10:44:59 -0400

    Update 01.sh

--- 02iskay/01.sh
+++ 02iskay/01.sh
@@ -1,36 +1,24 @@
 #!/bin/bash
-
 ## 01 ##
-
 ## IDENTIFY SDA
 # UEFI
 UefiN=1
-
 # swap
 SwapN=2
-
 # root
 RootN=3
-
-
 ## FORMAT PARTITIONS
 # UEFI
 mkfs.fat -F32 /dev/sda$UefiN
-
 # Swap
 mkswap /dev/sda$SwapN
-
 # Root
 mkfs.ext4 /dev/sda$RootN
-
-
 ## MOUNT PARTITIONS
 #UEFI
 mkdir /mnt/efi
 mount /dev/sda$UefiN /mnt/efi
-
 # Swap
 swapon /dev/sda$SwapN
-
 # Root
 mount /dev/sda$RootN /mnt
