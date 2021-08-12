85b05f6922afa9f92a877a30707960cc9b0d2106
hellparker <82740039+hellparker@users.noreply.github.com>
2021-08-12 10:46:41 -0400

    Update 04.sh

--- 02iskay/04.sh
+++ 02iskay/04.sh
@@ -1,27 +1,15 @@
 ##04##
-
 reflector --country Brazil,Argentina,Chile --sort rate -l 5 --save /etc/pacman.d/mirrorlist
-
 # Time zone
 ln -sf /usr/share/zoneinfo/America/La_Paz /etc/localtime
 hwclock --systohc
-
-
-
 ## Generating system locale
 nano /etc/locale.gen
-
 # Uncomment
 # en_US.UTF-8:
-
-
 # Generate the locale
 locale-gen
-
 # Select the system language
 localectl set-locale LANG=en_US.UTF-8
 echo LANG=en_US.UTF-8 > /etc/locale.conf
 echo KEYMAP=la-latin1 > /etc/vconsole.conf
-
-
-
