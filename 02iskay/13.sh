0a36ab6bde12ceb0f639e8d99ff5249d49e344b1
hellparker <82740039+hellparker@users.noreply.github.com>
2021-08-12 10:52:23 -0400

    Update 13.sh

--- 02iskay/13.sh
+++ 02iskay/13.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
-
 ##13##
-
 ## INSTALL & ENABLE NETWORK -AUDIO
-
 pacman -S wpa_supplicant wireless_tools networkmanager nm-connection-editor network-manager-applet  pavucontrol pulseaudio alsa-utils sof-firmware alsa-ucm-conf tlp ntfs-3g curl dialog
-
 systemctl enable NetworkManager.service
 systemctl enable tlp
 # systemctl enable fstrim.timer
-
 #systemctl enable lightdm.service
+
