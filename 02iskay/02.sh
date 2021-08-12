967c3071bf4329b7e3b8083c2cf9e2a3a2d175ad
hellparker <82740039+hellparker@users.noreply.github.com>
2021-08-12 10:45:18 -0400

    Update 02.sh

--- 02iskay/02.sh
+++ 02iskay/02.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-
 ##02##
-
 ## Mirrors
 reflector --country Brazil,Argentina,Chile --sort rate -l 10 --save /etc/pacman.d/mirrorlist
-
 ## Install Arch Linux
-pacstrap /mnt base linux linux-firmware intel-ucode rsync reflector nano git 
-
+pacstrap /mnt base linux linux-firmware intel-ucode rsync reflector nano git
