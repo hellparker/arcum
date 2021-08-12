cafa9e79d6bf1a60d9115bf3d93865607dba077e
hellparker <82740039+hellparker@users.noreply.github.com>
2021-08-12 10:48:24 -0400

    Update 06.sh

--- 02iskay/06.sh
+++ 02iskay/06.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
-
 ##06##
-
 ## REGULAR USER
-
 passwd
-
-useradd -m -G wheel,storage,power mechalar
-
-passwd mechalar
-
+useradd -m -G wheel,storage,power echalar
+passwd echalar
 EDITOR=nano visudo
-
 # Uncomment
 # %wheel ALL=(ALL) ALL
-
 
